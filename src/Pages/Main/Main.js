f93da773643c38c1d3831541a0e6c004d889cf93
jeongsooyoung <code.ameba@gmail.com>
2020-06-24 13:40:00 +0900

    Add: 내부 스크롤 형태의 아티클 컨테이너 스타일 구현

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Nav from '../../Components/Nav/Nav';
 import Footer from '../../Components/Footer/Footer';
+import './Main.scss';
 class Main extends React.Component {
   render() {
+    console.log('working');
     return (
       <div className="Main">
         <Nav />
+        <article>
+          <div></div>
+        </article>
         <Footer />
       </div>
     );
