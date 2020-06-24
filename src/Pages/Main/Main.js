aac9fbaa8717cd04c0b731e4b067aa63166f3267
jeongsooyoung <code.ameba@gmail.com>
2020-06-24 15:53:38 +0900

    Add: 메인페이지 자동반복재생 백그라운드 동영상 추가

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -4,12 +4,28 @@ import Footer from '../../Components/Footer/Footer';
 import './Main.scss';
 class Main extends React.Component {
   render() {
-    console.log('working');
+    const mainVideoUrl =
+      'http://www.chanel.com/videos/e_volume:mute/q_90,f_mp4,c_scale,w_2560,c_limit/FSH-1585840357798-hpmodedesktop.mp4';
     return (
       <div className="Main">
         <Nav />
         <article>
-          <div></div>
+          <div className="videoWrapper">
+            <video
+              autoPlay={true}
+              loop={true}
+              title="19bag"
+              width="100%"
+              height="100%"
+            >
+              <source src={mainVideoUrl} type="video/mp4" />
+            </video>
+            <div className="textWrapper">
+              <p>CHANEL 19 백</p>
+              <h4>부티크</h4>
+              <div>더보기</div>
+            </div>
+          </div>
         </article>
         <Footer />
       </div>
