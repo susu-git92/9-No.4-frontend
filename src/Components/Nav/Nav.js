a349ccedcad50c0f374daf67e528d4c6df1c8faf
jeongsooyoung <code.ameba@gmail.com>
2020-06-26 21:22:52 +0900

    Add: Search 컴포넌트 추가

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ModalCategory from '../ModalCategory/ModalCategory';
+import Search from '../Search/Search';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faStar, faUser } from '@fortawesome/free-solid-svg-icons';
 import './Nav.scss';
@@ -8,6 +9,7 @@ import { srcs } from '../../config';
 class Nav extends React.Component {
   state = {
     isActive: 0,
+    searchModal: false,
   };
   selectedMenu = (num) => {
     this.setState({
@@ -19,48 +21,66 @@ class Nav extends React.Component {
       isActive: 0,
     });
   };
+  SearchToggle = () => {
+    this.setState({
+      searchModal: !this.state.searchModal,
+    });
+  };
   render() {
     return (
       <div className="Nav">
-        <div className="NavWrapper">
-          <div className="boldLine" />
-          <img src={srcs.img.logo.black} alt="chanel logo" />
-          <div className="iconWrapper">
-            <FontAwesomeIcon className="searchIcon" icon={faSearch} />
-            <FontAwesomeIcon className="userIcon" icon={faUser} />
-            <FontAwesomeIcon className="starIcon" icon={faStar} />
+        {this.state.searchModal ? (
+          <div>
+            <div className="NavWrapper">
+              <div className="boldLine" />
+              <img src={srcs.img.logo.black} alt="chanel logo" />
+              <div className="iconWrapper">
+                <FontAwesomeIcon
+                  onClick={this.SearchToggle}
+                  className="searchIcon"
+                  icon={faSearch}
+                />
+                <FontAwesomeIcon className="userIcon" icon={faUser} />
+                <FontAwesomeIcon className="starIcon" icon={faStar} />
+              </div>
+              <ul>
+                <li
+                  className={this.state.isActive === 1 ? 'active' : 'none'}
+                  onClick={() => this.selectedMenu(1)}
+                >
+                  오뜨 꾸뛰르
+                </li>
+                <li
+                  className={this.state.isActive === 2 ? 'active' : 'none'}
+                  onClick={() => this.selectedMenu(2)}
+                >
+                  컬렉션
+                </li>
+                <li
+                  className={this.state.isActive === 3 ? 'active' : 'none'}
+                  onClick={() => this.selectedMenu(3)}
+                >
+                  카테고리
+                </li>
+                <li
+                  className={this.state.isActive === 4 ? 'active' : 'none'}
+                  onClick={() => this.selectedMenu(4)}
+                >
+                  CHANEL NEWS
+                </li>
+              </ul>
+            </div>
+            <ModalCategory
+              currentMenu={this.state.isActive}
+              onClick={this.closeModal}
+            />
           </div>
-          <ul>
-            <li
-              className={this.state.isActive === 1 ? 'active' : 'none'}
-              onClick={() => this.selectedMenu(1)}
-            >
-              오뜨 꾸뛰르
-            </li>
-            <li
-              className={this.state.isActive === 2 ? 'active' : 'none'}
-              onClick={() => this.selectedMenu(2)}
-            >
-              컬렉션
-            </li>
-            <li
-              className={this.state.isActive === 3 ? 'active' : 'none'}
-              onClick={() => this.selectedMenu(3)}
-            >
-              카테고리
-            </li>
-            <li
-              className={this.state.isActive === 4 ? 'active' : 'none'}
-              onClick={() => this.selectedMenu(4)}
-            >
-              CHANEL NEWS
-            </li>
-          </ul>
-        </div>
-        <ModalCategory
-          currentMenu={this.state.isActive}
-          onClick={this.closeModal}
-        />
+        ) : (
+          <Search
+            style={{ display: this.state.searchModal ? 'block' : 'none' }}
+            onClick={this.SearchToggle}
+          />
+        )}
       </div>
     );
   }
--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Main from './Pages/Main/Main';
+import Search from './Components/Search/Search';
 import Category from './Pages/Category/Category';
 import Collection from './Pages/Collection/Collection';
 import News from './Pages/News/News';
@@ -20,7 +21,8 @@ class Routes extends React.Component {
           <Route exact path="/news" component={News} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
-          <Route exact path="/Chanel19" component={Chanel19} />
+          <Route exact path="/chanel19" component={Chanel19} />
+          <Route exact path="/search" component={Search} />
         </Switch>
       </Router>
     );
