ce317e9a92709f2f9e6b284160881f43a50db977
jeongsooyoung <code.ameba@gmail.com>
2020-06-28 18:41:47 +0900

    Add: Nav에  mediaquery 추가하여 화면 너비 820px 이하일 때 변형

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ModalCategory from '../ModalCategory/ModalCategory';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faStar, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSearch,
+  faStar,
+  faUser,
+  faBars,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
 import './Nav.scss';
 import { srcs } from '../../config';
 
 class Nav extends React.Component {
   state = {
     isActive: 0,
+    mobileMenuActive: false,
   };
   selectedMenu = (num) => {
     this.setState({
@@ -19,18 +26,38 @@ class Nav extends React.Component {
       isActive: 0,
     });
   };
+  openMenu = () => {
+    this.setState({
+      mobileMenuActive: !this.state.mobileMenuActive,
+    });
+  };
   render() {
     return (
       <div className="Nav">
         <div className="NavWrapper">
           <div className="boldLine" />
           <img src={srcs.img.logo.black} alt="chanel logo" />
+          <FontAwesomeIcon
+            onClick={this.openMenu}
+            className="hamburgerBtn"
+            icon={faBars}
+          />
           <div className="iconWrapper">
             <FontAwesomeIcon className="searchIcon" icon={faSearch} />
             <FontAwesomeIcon className="userIcon" icon={faUser} />
             <FontAwesomeIcon className="starIcon" icon={faStar} />
           </div>
-          <ul>
+          <div
+            style={{ display: this.state.mobileMenuActive ? 'block' : 'none' }}
+            className="MobileMenuWrapper"
+          >
+            <MobileMenu
+              isActive={this.state.isActive}
+              onClick={() => this.selectedMenu}
+              mobileMenuActive={this.openMenu}
+            />
+          </div>
+          <ul className="desktopMenuList">
             <li
               className={this.state.isActive === 1 ? 'active' : 'none'}
               onClick={() => this.selectedMenu(1)}
@@ -61,6 +88,50 @@ class Nav extends React.Component {
           currentMenu={this.state.isActive}
           onClick={this.closeModal}
         />
+      </div>
+    );
+  }
+}
+
+class MobileMenu extends React.Component {
+  render() {
+    const { isActive, onClick, mobileMenuActive } = this.props;
+    return (
+      <div className="MobileMenu">
+        <div className="menuWrapper">
+          <p>메뉴</p>
+          <FontAwesomeIcon
+            onClick={mobileMenuActive}
+            className="closeBtn"
+            icon={faTimes}
+          />
+          <ul>
+            <li
+              className={isActive === 1 ? 'active' : 'none'}
+              onClick={() => onClick(1)}
+            >
+              오뜨 꾸뛰르
+            </li>
+            <li
+              className={isActive === 2 ? 'active' : 'none'}
+              onClick={() => onClick(2)}
+            >
+              컬렉션
+            </li>
+            <li
+              className={isActive === 3 ? 'active' : 'none'}
+              onClick={() => onClick(3)}
+            >
+              카테고리
+            </li>
+            <li
+              className={isActive === 4 ? 'active' : 'none'}
+              onClick={() => this.onClick(4)}
+            >
+              CHANEL NEWS
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
