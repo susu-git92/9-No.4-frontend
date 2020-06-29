fa79ae2261cf03686469f9bf0d7cc174d89a913a
jeongsooyoung <code.ameba@gmail.com>
2020-06-29 11:13:17 +0900

    Modify: 리팩토링, 모바일 메뉴창 열린 상태에서 화면 키웠을 떄 사라지지 않던 오류 수정

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ModalCategory from '../ModalCategory/ModalCategory';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faSearch,
@@ -8,38 +7,44 @@ import {
   faBars,
   faTimes,
 } from '@fortawesome/free-solid-svg-icons';
-import './Nav.scss';
+import ModalCategory from '../ModalCategory/ModalCategory';
 import { srcs } from '../../config';
+import './Nav.scss';
 
 class Nav extends React.Component {
   state = {
     isActive: 0,
     mobileMenuActive: false,
   };
+
   selectedMenu = (num) => {
     this.setState({
       isActive: num,
     });
   };
+
   closeModal = () => {
     this.setState({
       isActive: 0,
     });
   };
-  openMenu = () => {
+
+  handleMobileMenu = () => {
     this.setState({
       mobileMenuActive: !this.state.mobileMenuActive,
     });
   };
+
   render() {
+    const { isActive, mobileMenuActive } = this.state;
     return (
       <div className="Nav">
         <div className="NavWrapper">
           <div className="boldLine" />
           <img src={srcs.img.logo.black} alt="chanel logo" />
           <FontAwesomeIcon
-            onClick={this.openMenu}
             className="hamburgerBtn"
+            onClick={this.handleMobileMenu}
             icon={faBars}
           />
           <div className="iconWrapper">
@@ -48,36 +53,36 @@ class Nav extends React.Component {
             <FontAwesomeIcon className="starIcon" icon={faStar} />
           </div>
           <div
-            style={{ display: this.state.mobileMenuActive ? 'block' : 'none' }}
             className="MobileMenuWrapper"
+            style={{ display: mobileMenuActive ? 'block' : 'none' }}
           >
             <MobileMenu
-              isActive={this.state.isActive}
-              onClick={() => this.selectedMenu}
-              mobileMenuActive={this.openMenu}
+              isActive={isActive}
+              handleSelectedMobileMenu={() => this.selectedMenu}
+              mobileMenuActive={this.handleMobileMenu}
             />
           </div>
           <ul className="desktopMenuList">
             <li
-              className={this.state.isActive === 1 ? 'active' : 'none'}
+              className={isActive === 1 ? 'active' : 'none'}
               onClick={() => this.selectedMenu(1)}
             >
               오뜨 꾸뛰르
             </li>
             <li
-              className={this.state.isActive === 2 ? 'active' : 'none'}
+              className={isActive === 2 ? 'active' : 'none'}
               onClick={() => this.selectedMenu(2)}
             >
               컬렉션
             </li>
             <li
-              className={this.state.isActive === 3 ? 'active' : 'none'}
+              className={isActive === 3 ? 'active' : 'none'}
               onClick={() => this.selectedMenu(3)}
             >
               카테고리
             </li>
             <li
-              className={this.state.isActive === 4 ? 'active' : 'none'}
+              className={isActive === 4 ? 'active' : 'none'}
               onClick={() => this.selectedMenu(4)}
             >
               CHANEL NEWS
@@ -85,8 +90,8 @@ class Nav extends React.Component {
           </ul>
         </div>
         <ModalCategory
-          currentMenu={this.state.isActive}
-          onClick={this.closeModal}
+          currentMenu={isActive}
+          handleCloseBtn={this.closeModal}
         />
       </div>
     );
@@ -95,38 +100,38 @@ class Nav extends React.Component {
 
 class MobileMenu extends React.Component {
   render() {
-    const { isActive, onClick, mobileMenuActive } = this.props;
+    const { isActive, mobileMenuActive, handleSelectedMobileMenu } = this.props;
     return (
       <div className="MobileMenu">
         <div className="menuWrapper">
           <p>메뉴</p>
           <FontAwesomeIcon
-            onClick={mobileMenuActive}
             className="closeBtn"
+            onClick={mobileMenuActive}
             icon={faTimes}
           />
-          <ul>
+          <ul className="mobileMenuList">
             <li
               className={isActive === 1 ? 'active' : 'none'}
-              onClick={() => onClick(1)}
+              onClick={() => handleSelectedMobileMenu(1)}
             >
               오뜨 꾸뛰르
             </li>
             <li
               className={isActive === 2 ? 'active' : 'none'}
-              onClick={() => onClick(2)}
+              onClick={() => handleSelectedMobileMenu(2)}
             >
               컬렉션
             </li>
             <li
               className={isActive === 3 ? 'active' : 'none'}
-              onClick={() => onClick(3)}
+              onClick={() => handleSelectedMobileMenu(3)}
             >
               카테고리
             </li>
             <li
               className={isActive === 4 ? 'active' : 'none'}
-              onClick={() => this.onClick(4)}
+              onClick={() => handleSelectedMobileMenu(4)}
             >
               CHANEL NEWS
             </li>
