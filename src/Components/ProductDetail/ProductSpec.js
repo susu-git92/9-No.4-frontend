2e33ec21f877a3bf3747b7cc71186e288502bd12
ljjun <gwejja@gmail.com>
2020-06-29 13:23:55 +0900

    Modify: unit 부분 구현

--- src/Components/ProductDetail/ProductSpec.js
+++ src/Components/ProductDetail/ProductSpec.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class ProductSpec extends Component {
+  state = {
+    size: '16 × 26 × 9 cm',
+    unit: 'in',
+  };
+
+  unitClick = () => {
+    if (this.state.unit === 'in') {
+      this.setState({ size: '6.3 × 10.2 × 3.5 in', unit: 'cm' });
+    }
+    if (this.state.unit === 'cm') {
+      this.setState({ size: '16 × 26 × 9 cm', unit: 'in' });
+    }
+  };
+
   render() {
     return (
       <div className="detail_box">
@@ -9,8 +23,14 @@ class ProductSpec extends Component {
         </div>
         <div className="item_color">네이비 블루, 화이트, 레드</div>
         <div className="item_size">
-          16 × 26 × 9 cm
-          <span className="unit_change">(in)</span>
+          {this.state.size}
+          <span className="unit_box">
+            (
+            <span className="unit_change" onClick={this.unitClick}>
+              {this.state.unit}
+            </span>
+            )
+          </span>
         </div>
 
         <div className="reference">레퍼런스: AS1160 B02877 MH694</div>
