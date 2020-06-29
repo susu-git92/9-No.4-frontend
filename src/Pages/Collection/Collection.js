2284b19adfa518ae0d0a2e44fac13abd42b1b232
jeongsooyoung <code.ameba@gmail.com>
2020-06-29 20:24:50 +0900

    Modify: collection 컴포넌트 초기세팅

--- src/Pages/Collection/Collection.js
+++ src/Pages/Collection/Collection.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Nav from '../../Components/Nav/Nav';
+import Footer from '../../Components/Footer/Footer';
+import './Collection.scss';
 
 class Collection extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Collection Page</h1>
-      </div>
+      <article className="Collection">
+        <Nav />
+        <div className="collectionWrapper"></div>
+        <Footer />
+      </article>
     );
   }
 }
