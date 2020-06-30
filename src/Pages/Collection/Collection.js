2bd69d475206ef204d47305d9fe9c10b16e56a45
jeongsooyoung <code.ameba@gmail.com>
2020-06-30 12:40:23 +0900

    Add: collection page 생성

--- src/Pages/Collection/Collection.js
+++ src/Pages/Collection/Collection.js
@@ -1,14 +1,26 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable array-callback-return */
 import React from 'react';
 import Nav from '../../Components/Nav/Nav';
 import Footer from '../../Components/Footer/Footer';
+import { srcs } from '../../config.js';
 import './Collection.scss';
 
 class Collection extends React.Component {
   render() {
+    const collectionImgs = srcs.img.collection;
     return (
       <article className="Collection">
         <Nav />
-        <div className="collectionWrapper"></div>
+        <div className="collectionWrapper">
+          <div>
+            {collectionImgs.map((img) => (
+              <div className="imgWrapper" key={img.id}>
+                <img src={img.url} alt="img" />
+              </div>
+            ))}
+          </div>
+        </div>
         <Footer />
       </article>
     );
--- src/config.js
+++ src/config.js
@@ -18,7 +18,110 @@ export const srcs = {
         'https://www.chanel.com/images/q_auto,f_auto,fl_lossy,dpr_auto/w_450/FSH-1585566665375-megamenuhv4.jpg',
     },
     news: {
-      newsLetterLogo: 'https://www.chanel.com/asset/frontstage/img/chanel-news.gif',
-    }
+      newsLetterLogo:
+        'https://www.chanel.com/asset/frontstage/img/chanel-news.gif',
+    },
+    collection: [
+      {
+        id: 1,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-1-8821844934686.jpg',
+      },
+      {
+        id: 2,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-2-8821844672542.jpg',
+      },
+      {
+        id: 3,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-3-8821845295134.jpg',
+      },
+      {
+        id: 4,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-4-8821843951646.jpg',
+      },
+      {
+        id: 5,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-5-8821843558430.jpg',
+      },
+      {
+        id: 6,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-6-8821843361822.jpg',
+      },
+      {
+        id: 7,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-7-8821843197982.jpg',
+      },
+      {
+        id: 8,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-8-8821845360670.jpg',
+      },
+      {
+        id: 9,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-9-8821843787806.jpg',
+      },
+      {
+        id: 10,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-10-8821844410398.jpg',
+      },
+      {
+        id: 11,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-11-8821845393438.jpg',
+      },
+      {
+        id: 12,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-12-8821844279326.jpg',
+      },
+      {
+        id: 13,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-13-8821845196830.jpg',
+      },
+      {
+        id: 14,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-14-8821843591198.jpg',
+      },
+      {
+        id: 15,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-15-8821843066910.jpg',
+      },
+      {
+        id: 16,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-16-8821845753886.jpg',
+      },
+      {
+        id: 17,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-17-8821844017182.jpg',
+      },
+      {
+        id: 18,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-18-8821844869150.jpg',
+      },
+      {
+        id: 19,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-19-8821844639774.jpg',
+      },
+      {
+        id: 20,
+        url:
+          'https://www.chanel.com/images/f_auto,q_auto,c_scale,w_4096/c_crop,h_3015,w_2040,x_1028,y_148/q_auto,f_auto,fl_lossy,dpr_auto/w_372/cruise-2019-20-20-8821844246558.jpg',
+      },
+    ],
   },
 };
