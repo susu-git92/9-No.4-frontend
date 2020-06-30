3534ce768b459ddba5bb86ca8c2388fd6503befa
jeongsooyoung <code.ameba@gmail.com>
2020-06-30 19:46:23 +0900

    Add: 컬렉션 상품 디테일 페이지 생성

---
+++ src/Pages/Collection/CollectionDetail/CollectionDetail.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Nav from '../../../Components/Nav/Nav';
+import Footer from '../../../Components/Footer/Footer';
+import './CollectionDetail.scss';
+
+class CollectionDetail extends React.Component {
+  state = {
+    products: {},
+  };
+  componentDidMount() {
+    fetch('http://10.58.0.55:8000/product/cruise-2019-20')
+      .then((res) => res.json())
+      .then((res) =>
+        this.setState({
+          products: res,
+        }),
+      );
+  }
+  render() {
+    console.log(this.state.products);
+    return (
+      <article className="CollectionDetail">
+        <Nav />
+        <div className="productContainer">detail page</div>
+        {/* <Footer /> */}
+      </article>
+    );
+  }
+}
+
+export default CollectionDetail;
