b3c5381bd32e21a1d59a2fafdf296d0eeb1ab9c1
jeongsooyoung <code.ameba@gmail.com>
2020-06-30 17:15:36 +0900

    Modify: mock data 이용하여 렌더링 확인

--- src/Pages/Collection/Collection.js
+++ src/Pages/Collection/Collection.js
@@ -1,22 +1,38 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable array-callback-return */
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import Nav from '../../Components/Nav/Nav';
 import Footer from '../../Components/Footer/Footer';
 import { srcs } from '../../config.js';
 import './Collection.scss';
 
 class Collection extends React.Component {
+  state = {
+    collectionItems: {},
+  };
+
+  // componentDidMount() {
+  //   fetch('http://10.58.0.55:8000/product/cruise-2019-20')
+  //     .then((res) => res.json())
+  //     .then((res) => this.setState({ products: res }));
+  // }
+
   render() {
-    const collectionImgs = srcs.img.collection;
+    const collectionItems = srcs.img.collection;
     return (
       <article className="Collection">
         <Nav />
         <div className="collectionWrapper">
           <div>
-            {collectionImgs.map((img) => (
-              <div className="imgWrapper" key={img.id}>
-                <img src={img.url} alt="img" />
+            {collectionItems.map((item, idx) => (
+              <div className="itemsContainer" key={item[idx + 1].name}>
+                <div className="nameWrapper">
+                  {item[idx + 1].name}
+                  <FontAwesomeIcon icon={faStar} />
+                </div>
+                <div className="imgWrapper">
+                  <img src={item[idx + 1].img} alt="img" />
+                </div>
               </div>
             ))}
           </div>
--- src/Routes.js
+++ src/Routes.js
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './Pages/Main/Main';
 import Category from './Pages/Category/Category';
 import Collection from './Pages/Collection/Collection';
-// import News from './Pages/News/News';
-// import Loginscreen from './Pages/Login/Loginscreen';
-// import Login from './Pages/Login/Login/Login';
-// import Signup from './Pages/Login/Signup/Signup';
+import News from './Pages/News/News';
+import Loginscreen from './Pages/Login/Loginscreen';
 import Chanel19 from './Pages/Chanel19/Chanel19';
 import Chanel19Detail from './Pages/Chanel19/Chanel19Detail/Chanel19Detail';
 
@@ -18,12 +16,10 @@ class Routes extends React.Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/category" component={Category} />
           <Route exact path="/collection" component={Collection} />
-          {/* <Route exact path="/news" component={News} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} /> */}
+          <Route exact path="/news" component={News} />
           <Route exact path="/chanel19" component={Chanel19} />
           <Route exact path="/chanel19detail" component={Chanel19Detail} />
-          {/* <Route exact path="/loginscreen" component={Loginscreen} /> */}
+          <Route exact path="/loginscreen" component={Loginscreen} />
         </Switch>
       </Router>
     );
