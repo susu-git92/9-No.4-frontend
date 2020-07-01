10876eeefa1d8ce4caebd73ae29c8d4571b3316c
jeongsooyoung <code.ameba@gmail.com>
2020-07-01 20:03:47 +0900

    Modify: mock data로 대체

--- src/Pages/Collection/Collection.js
+++ src/Pages/Collection/Collection.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import Nav from '../../Components/Nav/Nav';
 import Footer from '../../Components/Footer/Footer';
-// import { srcs } from '../../config.js';
+import { srcs } from '../../config.js';
 import './Collection.scss';
 
 class Collection extends React.Component {
@@ -13,17 +13,17 @@ class Collection extends React.Component {
   };
 
   componentDidMount() {
-    fetch('http://10.58.0.55:8000/product/cruise-2019-20')
-      .then((res) => res.json())
-      .then((res) => this.setState({ collectionItems: res.look }));
+    // fetch('http://10.58.0.55:8000/product/cruise-2019-20')
+    //   .then((res) => res.json())
+    //   .then((res) => this.setState({ collectionItems: res.look }));
 
-    // this.setState({
-    //   collectionItems: srcs.img.look,
-    // });
+    this.setState({
+      collectionItems: srcs.img.look,
+    });
   }
 
   // addWishList = (itemId) => {
-  //   fetch(`http://10.58.0.55:8000/product/cruise-2019-20/${itemId}/`, {
+  //   fetch(`http://10.58.0.55:8000/product/cruise-2019-20/${itemId}`, {
   //     method: 'POST',
   //     headers: {
   //       'Content-Type': 'application/json',
--- src/Pages/Collection/CollectionDetail/CollectionDetail.js
+++ src/Pages/Collection/CollectionDetail/CollectionDetail.js
@@ -1,31 +1,67 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChevronLeft,
+  faChevronRight,
+  faStar,
+} from '@fortawesome/free-solid-svg-icons';
 import Nav from '../../../Components/Nav/Nav';
 import Footer from '../../../Components/Footer/Footer';
 import './CollectionDetail.scss';
+import { srcs } from '../../../config';
 
 class CollectionDetail extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      currentProduct: Number(this.props.match.params.id),
+      productsId: Number(this.props.match.params.id),
+      productImages: [],
+      productInfo: [],
     };
   }
 
   componentDidMount() {
-    fetch(
-      `http://10.58.0.55:8000/product/cruise-2019-20/${this.state.currentProduct}/`,
-    )
-      .then((res) => res.json())
-      .then((res) => console.log(res));
+    // fetch(
+    //   `http://10.58.0.55:8000/product/cruise-2019-20/${this.state.productsId}`,
+    // )
+    //   .then((res) => res.json())
+    //   .then((res) =>
+    //     this.setState({ productImages: res.img, productInfo: res.products }),
+    //   );
+    this.setState({
+      productImages: srcs.img.currentProduct.img,
+      productInfo: srcs.img.currentProduct.products,
+    });
   }
 
   render() {
-    console.log(this.props);
+    const { productImages, productInfo, productsId } = this.state;
     return (
       <article className="CollectionDetail">
         <Nav />
-        <div className="productContainer">detail page</div>
+        <div className="productContainer">
+          {productImages.map((img) => (
+            <div className="productWrapper" key={img}>
+              <div className="imgWrapper">
+                <img src={img} alt="look img" />
+              </div>
+            </div>
+          ))}
+          <div className="textWrapper">
+            <div className="boldLine">
+              <h4>룩 {productsId}</h4>
+              <div className="iconWrapper">
+                <FontAwesomeIcon className="prevIcon" icon={faChevronLeft} />
+                <FontAwesomeIcon className="nextIcon" icon={faChevronRight} />
+              </div>
+            </div>
+            <div>
+              <span>크루즈 2019/20</span>
+              <FontAwesomeIcon className="starIcon" icon={faStar} />
+            </div>
+          </div>
+        </div>
         <Footer />
       </article>
     );
