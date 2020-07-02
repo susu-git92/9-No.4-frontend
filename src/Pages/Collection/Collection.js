eeb2e39bdf10ee7769f4048f334b622fe1bbfd2c
jeongsooyoung <code.ameba@gmail.com>
2020-07-02 16:18:35 +0900

    Add: POST 성공, 위시리스트 구현 시작

--- src/Pages/Collection/Collection.js
+++ src/Pages/Collection/Collection.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import Nav from '../../Components/Nav/Nav';
 import Footer from '../../Components/Footer/Footer';
-import { srcs } from '../../config.js';
+// import { srcs } from '../../config.js';
 import './Collection.scss';
 
 class Collection extends React.Component {
@@ -13,24 +13,38 @@ class Collection extends React.Component {
   };
 
   componentDidMount() {
-    // fetch('http://10.58.0.55:8000/product/cruise-2019-20')
-    //   .then((res) => res.json())
-    //   .then((res) => this.setState({ collectionItems: res.look }));
+    fetch('http://10.58.0.55:8000/product/cruise-2019-20')
+      .then((res) => res.json())
+      .then((res) => this.setState({ collectionItems: res.look }));
 
-    this.setState({
-      collectionItems: srcs.img.look,
-    });
+    // this.setState({
+    //   collectionItems: srcs.img.look,
+    // });
+
+    localStorage.setItem(
+      'user',
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImludHplcm9Ad2Vjb2RlLmNvbSJ9.kkMVMG0hgqywiz81AihEs6syYkB7kDC1MHf1YfwcB0I',
+    );
   }
 
-  // addWishList = (itemId) => {
-  //   fetch(`http://10.58.0.55:8000/product/cruise-2019-20/${itemId}`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(itemId),
-  //   }).then((res) => res.json());
-  // };
+  addWishList = (item) => {
+    console.log(item.id);
+    const userToken = localStorage.getItem('user');
+    fetch(`http://10.58.0.55:8000/product/wishlist/look/${item.id}/`, {
+      method: 'post',
+      headers: {
+        Authorization: userToken,
+      },
+    })
+      .then((res) => {
+        if (res.statue === 200 || res.status === 201) {
+          res.json().then((json) => console.log(json));
+        } else {
+          console.log(res.status);
+        }
+      })
+      .catch((err) => console.error(err));
+  };
 
   render() {
     const { collectionItems } = this.state;
@@ -42,18 +56,24 @@ class Collection extends React.Component {
         <div className="collectionWrapper">
           <div>
             {collectionItems.map((item) => (
-              <div
-                className="itemsContainer"
-                key={item.id}
-                onClick={() => this.props.history.push(`/product/${item.id}`)}
-              >
+              <div className="itemsContainer" key={item.id}>
                 <div className="nameWrapper">
                   {item.id}
-                  <FontAwesomeIcon className="starIcon" icon={faStar} />
+                  <FontAwesomeIcon
+                    onClick={() => this.addWishList(item)}
+                    className="starIcon"
+                    icon={faStar}
+                  />
                 </div>
                 <Link to={`/product/${item.id}`}>
                   <div className="imgWrapper">
-                    <img src={item.image} alt="img" />
+                    <img
+                      src={item.image}
+                      alt="img"
+                      onClick={() =>
+                        this.props.history.push(`/product/${item.id}`)
+                      }
+                    />
                   </div>
                 </Link>
               </div>
