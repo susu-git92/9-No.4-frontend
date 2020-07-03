f275c5a95a12afdc2244db271ca952f52fef23e0
susu-git92 <chlojeong@gamil.com>
2020-07-03 15:06:23 +0900

    modify : 위시리스트 로그인하지 않은 경우, 로그인 페이지로 이동하기

--- src/Pages/WishList/WishList.js
+++ src/Pages/WishList/WishList.js
@@ -14,8 +14,10 @@ export class WishList extends React.Component {
     };
   }
 
-   componentDidMount () {
-    fetch('http://10.58.0.55:8000/account/wishlist/',
+  componentDidMount () {
+    const token = localStorage.getItem('AccessToken')
+    if(token) {
+      fetch('http://10.58.0.55:8000/account/wishlist/',
       {
         method: 'GET',
         headers: {Authorization: localStorage.getItem('AccessToken')
@@ -24,15 +26,24 @@ export class WishList extends React.Component {
     )
       .then((res) => res.json())
       .then((res) => this.setState({productListArr : res.prod_wishlist}))
+    } else {
+      alert("로그인을 먼저 진행해주세요");
+      this.props.history.push('/loginscreen');
+    }
   }
   
   render() {
    console.log("productListArr >>> ",this.state.productListArr)
+   const { productListArr } = this.state; // destructuring
     return (
       <div className="WishList">
         <Nav/> 
         <div className="wishlist-container">
-          {this.state.productListArr.length > 0 ? <WishViewer products={this.state.productListArr} /> : <WishEmpty/>}
+          {
+            productListArr.length !== 0 
+            ? <WishViewer products={productListArr} /> 
+            : <WishEmpty/>
+          }
         </div>
         <Footer/> 
       </div>
